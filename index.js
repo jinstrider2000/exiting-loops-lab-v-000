fead5318c2ceefdbaedf7623e21da13b31d114dc
Efrain Perez Jr <efrainperezjr@live.com>
2018-03-19 22:19:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   for (var i = 0; i < array.length; i++) {
-    if (array[i] === findFn(array[i])) {
-      return findFn(array[i]);
+    if (findFn(array[i])) {
+      return array[i];
     }
     else {
       continue;
